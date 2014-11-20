df7dc64e8acd45313fc8014b27f4343360c4836e
RNencib <rnencib@esrf.fr>
2014-11-20 09:44:27 +0100

    Update Window.py

--- Window.py
+++ Window.py
@@ -10,62 +10,55 @@ class SimpleGUI(QMainWindow):
         self.initUI()
         self.tab_widget = QTabWidget(self)
         self.setCentralWidget(self.tab_widget)
+        self.table = Table(self)
+
     def initUI(self):  
-        #create the open file action
+
         openFile = QAction('Open', self)
-        openFile.setShortcut('Ctrl+O') # Create shortcut
-        openFile.setStatusTip('Open new File') # definition of the ation 
-        openFile.triggered.connect(self.ShowFile)# use the ShowFile method
-        # create the save file action
+        openFile.setShortcut('Ctrl+O') 
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.ShowFile)
+
+       
         saveFile = QAction('Save', self)
         saveFile.setShortcut('Ctrl+S')
         saveFile.setStatusTip('Save File')
-        saveFile.triggered.connect(self.Save)# use the Save method
-        #Create new configuration
+        saveFile.triggered.connect(self.Save)
+
+
         Create = QAction('Create', self)
         Create.setStatusTip('Create new configuration')
         Create.triggered.connect(self.New_Config)
-        # create menu
+
         menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')# add the file section on the menu
+        fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(openFile)
         fileMenu.addAction(saveFile)
         fileMenu = menubar.addMenu('&New Configuration')
         fileMenu.addAction(Create)
-        # create window
-        self.setGeometry(300, 300,500,500)# position and size of the window
+
+        self.setGeometry(300, 300,500,500)
         self.setWindowTitle('Binoculars')
-        self.setWindowIcon(QIcon('binoculars.png'))# the icon of the app
+        self.setWindowIcon(QIcon('binoculars.png'))
         self.show() 
-    # open file method  
+
     def ShowFile(self):
-        #fname = QFileDialog.getOpenFileName(self, 'Open file','/home')
-        #f = open(fname, 'r') # show the text file on the window
-        # edit the text
-        #with f:        
-            #data = f.read()
-            #self.textEdit.setText(data) 
-        path = QFileDialog.getOpenFileName(
-                self, 'Open File', '', 'CSV(*.csv)')
+        path = QFileDialog.getOpenFileName(self, 'Open File', '', '*.txt')
         if not path.isEmpty():
-            with open(unicode(path), 'rb') as stream:
-                self.table.setRowCount(0)
-                self.table.setColumnCount(0)
-                for rowdata in csv.reader(stream):
+                self.table.setRowCount(1)
+                self.table.setColumnCount(3)
+                for rowdata in data.readlines():
                     row = self.table.rowCount()
                     self.table.insertRow(row)
                     self.table.setColumnCount(len(rowdata))
                     for column, data in enumerate(rowdata):
                         item = QTableWidgetItem(data.decode('utf8'))
                         self.table.setItem(row, column, item)
-    # save file method
+
     def Save(self):
-        #fsave = QFileDialog.getSaveFileName(self,'Save file','/home')
-        path = QFileDialog.getSaveFileName(
-            self, 'Save File', '', 'CSV(*.csv)')
+        path = QFileDialog.getSaveFileName(self, 'Save File', '', '*.txt')
         if not path.isEmpty():
-            with open(unicode(path), 'wb') as stream:
-                writer = csv.writer(stream)
+                writer = file.write()
                 for row in range(self.table.rowCount()):
                     rowdata = []
                     for column in range(self.table.columnCount()):
@@ -89,39 +82,35 @@ class SimpleGUI(QMainWindow):
 #-----------------------------------------CREATE TABLE-----------------------------------------------       
 
 
-class table(QWidget):
+class Table(QWidget):
 
     
     def __init__(self, parent = None):
-        super(table, self).__init__()
+        super(Table, self).__init__()
         
         
         # create a QTableWidget 
-        
-        self.table = QTableWidget(1, 2, self)
-        self.table.setHorizontalHeaderLabels(['Parameter', 'Value'])
+        self.table = QTableWidget(1, 3, self)
+        self.table.setHorizontalHeaderLabels(['Parameter', 'Value','Comment'])
         self.table.horizontalHeader().setStretchLastSection(True)
         self.table.verticalHeader().setVisible(False)
         #create combobox
         combobox = QComboBox()
-        list = []
-        combobox.addItems(QStringList(self.list))
+        choise = []
+        combobox.addItems(QStringList(self.choise))
         #add items
         cell = QTableWidgetItem(QString("Types"))
         self.table.setItem(0, 0, cell)
         self.table.setCellWidget(0, 1, combobox)
-        # create push button and add Types
+
         self.btn_add_types = QPushButton('Add Type', self)
-        # create text edit
         self.TypeEdit = QLineEdit(self)
         #self.connect(self.btn_add_types, SIGNAL('clicked()'), self.add_types)
-        # create push button and add it to a horizontal layout
+
         self.btn_add_row = QPushButton('+', self)
-        # connect button clicked signal to our handler
         self.connect(self.btn_add_row, SIGNAL('clicked()'), self.add_row)
-        # create push button and delete it to a horizontal layout
+
         self.btn_del_row = QPushButton('-', self)
-        # connect button clicked signal to our handler
         self.connect(self.btn_del_row, SIGNAL('clicked()'), self.del_row)
           
 
@@ -150,17 +139,17 @@ class table(QWidget):
         
        
 
-class Dispatcher(table):
+class Dispatcher(Table):
     #label = QLabel('Dispatcher')#bug
-    list = ['Local','OAR',]
+    choise = ['Local','OAR',]
     
-class Input(table):
+class Input(Table):
     #label = QLabel('Input')#bug
-    list = []
+    choise = []
     
-class Projection(table):
+class Projection(Table):
     #label = QLabel('Projection')#bug
-    list = []
+    choise = []
     
 #----------------------------------------------------------------------------------------------------
         
